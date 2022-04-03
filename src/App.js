29ff66f80ec2899e37631e769ce5ef609c4462f2
chaima-sassi <sassiichaima@gmail.com>
2022-04-03 22:29:52 +0100

    checkpointjsx

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import image1 from "./myImage.png"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   
+      <div style={{borderSolid:"1px black",maxWidth:"100vw"}}>
+
+         <h1 className='title red' >chaima</h1>
+
+        <br/>
+
+       <img src={image1} alt='myImage'/>
+
+       <br/>
+
+       <img src="/image2.jpg" alt='image2' />
+<br/>
+
+       <iframe width="320" height="240" src="https://www.youtube.com/embed/J7Iz5060PUE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
     </div>
   );
 }
